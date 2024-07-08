f323b71595febcc0404153a137972560f5222bdb
giorg22 <giorgibakhturidze2202@gmail.com>
2024-07-08 23:55:35 +0400

    added unit tests

--- UnitTests/SlotServiceTests.cs
+++ UnitTests/SlotServiceTests.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Application.Models;
+using Application.Slots.Models;
 using Application.Slots.Services;
 using Application.Shared;
 using Domain.Entities;
